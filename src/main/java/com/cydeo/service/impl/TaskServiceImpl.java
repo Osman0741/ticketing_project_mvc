f15f7202bafb9bcff6b7cc60925122ca763efebe
LENOVO <osmnozdmrosh@gmail.com>
2023-08-28 10:31:51 +0300

    Ticketing Project,Task Page Completed

---
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -0,0 +1,65 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskServiceImpl extends AbstractMapService<TaskDTO, Long> implements TaskService {
+    @Override
+    public TaskDTO save(TaskDTO task) {
+
+        if(task.getTaskStatus()==null){
+            task.setTaskStatus(Status.OPEN);
+        }
+        if (task.getAssignedDate()==null){
+            task.setAssignedDate(LocalDate.now());
+        }
+
+        if (task.getId()==null){
+            task.setId(UUID.randomUUID().getMostSignificantBits());
+        }
+        return super.save(task.getId(), task);
+    }
+
+    @Override
+    public TaskDTO findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public List<TaskDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void update(TaskDTO task) {
+
+        if(task.getTaskStatus()==null){
+            task.setTaskStatus(Status.OPEN);
+        }
+        if (task.getAssignedDate()==null){
+            task.setAssignedDate(LocalDate.now());
+        }
+        super.update(task.getId(), task);
+    }
+
+    @Override
+    public List<TaskDTO> findTasksByManager(UserDTO manager) {
+        return findAll().stream().filter(task->task.getProject().getAssignedManager().equals(manager))
+                .collect(Collectors.toList());
+    }
+}
